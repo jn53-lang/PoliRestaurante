cf7ee095f8f5a021f5129f22859893b22926e0bf
Stuve <Stuve@DESKTOP-52SL1TQ>
2026-09-22 13:36:21 -0500

    Primer Commit

---
+++ src/main/java/com/co/poligran/polirestaurante/Front/PoliRestaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.co.poligran.polirestaurante.Front;
+
+/**
+ *
+ * @author Stuve
+ */
+public class PoliRestaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
