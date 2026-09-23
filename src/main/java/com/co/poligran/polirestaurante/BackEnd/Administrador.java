a1e629c76d314ad12c50e289aa13a7fc0875de57
Stuve <Stuve@DESKTOP-52SL1TQ>
2026-09-22 19:18:23 -0500

    Tercer Commit
    Falta agregar mas clases

--- src/main/java/com/co/poligran/polirestaurante/BackEnd/Administrador.java
+++ src/main/java/com/co/poligran/polirestaurante/BackEnd/Administrador.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.co.poligran.polirestaurante.BackEnd;
 
-/**
- *
- * @author salaG201
- */
+
 public class Administrador extends Usuario {
     
     public Administrador(int id, String nombre, String correo, String password, String rol) {
--- src/main/java/com/co/poligran/polirestaurante/Front/PoliRestaurante.java
+++ src/main/java/com/co/poligran/polirestaurante/Front/PoliRestaurante.java
@@ -4,6 +4,8 @@
 
 package com.co.poligran.polirestaurante.Front;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Stuve
@@ -11,6 +13,27 @@ package com.co.poligran.polirestaurante.Front;
 public class PoliRestaurante {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+                Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bienvenido al Poli Restaurante");
+        System.out.println("Si es usuario frecuente recomandamos que inicie sesion, si no tiene una, creela");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println();
+        System.out.println("Digite 1 para iniciar sesion");
+        System.out.println("Digite 2 para crear usuario");
+        
+        int login = 2; 
+        String usuario;
+        String password;
+        switch (login){
+        case 1 -> {
+            System.out.println(" Ingrese su usuario: ");
+            usuario = sc.next();
+            System.out.println(" Ingrese su contraseña: ");
+            password = sc.next();
+            }
+        
+        
+    }
     }
 }
